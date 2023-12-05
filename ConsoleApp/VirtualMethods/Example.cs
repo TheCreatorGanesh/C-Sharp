413b9ac60c60958c69c2d7e2872b6c64b03f6cca
The_Creator <ganeshm995@hotmail.com>
2023-12-05 19:18:24 +0530

    Added virtual functions example

---
+++ ConsoleApp/VirtualMethods/Example.cs
@@ -0,0 +1,30 @@
+﻿namespace VirtualMethods
+{
+    public class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("BASE: Print()");
+        }
+
+        public void Printing()
+        {
+            Console.WriteLine("BASE: Printing()");
+        }
+
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("DERIVED: Print()");
+        }
+
+        public void Printing()
+        {
+            Console.WriteLine("DERIVED: Printing()");
+        }
+
+    }
+}
