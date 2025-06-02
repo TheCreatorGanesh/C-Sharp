1b1fd79eca4abebfbfbe3b718d823b9d2af5c42d
The_Creator <ganeshm995@hotmail.com>
2025-06-02 18:17:25 +0530

    Added coding solutions project

---
+++ CodingSolutions/Functions.cs
@@ -0,0 +1,33 @@
+﻿namespace CodingSolutions
+{
+	public static class Functions
+	{
+		/// <summary>
+		/// Prints the number of occurrences for each non-space character in the sentence.
+		/// Ignores spaces and outputs the count for each character to the console.
+		/// </summary>
+		/// <param name="sentence">The input string to analyze.</param>
+		public static void PrintLetterOccurrencesInString(this string sentence)
+		{
+			Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+			for (int i = 0; i < sentence.Length; i++)
+			{
+				if (charCount.ContainsKey(sentence[i]))
+				{
+					charCount[sentence[i]]++;    // Increment if char already exists
+				}
+				else
+				{
+					if (sentence[i] != ' ')  // Add if char does not exist and is not a space
+						charCount.Add(sentence[i], 1);
+				}
+			}
+
+			foreach (var item in charCount)
+			{
+				Console.WriteLine($"Letter {item.Key} occured {item.Value} time(s).");
+			}
+		}
+	}
+}
---
+++ CodingSolutions/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using CodingSolutions;
+
+#region Prints the number of occurrences for each non-space character in the sentence.
+string str = "Ganesh lives in india";
+str.PrintLetterOccurrencesInString();
+#endregion
