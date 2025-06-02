35b0977dbe00ace4ef09b008959f3fe94367b171
The_Creator <ganeshm995@hotmail.com>
2025-06-02 18:20:05 +0530

    Added Virtual Methods project

---
+++ VirtualMethods/Data.cs
@@ -0,0 +1,30 @@
+﻿namespace VirtualMethods
+{
+	public class BaseClass
+	{
+		public virtual void Print()
+		{
+			Console.WriteLine("BASE: Print() - virtual method");
+		}
+
+		public void Printing()
+		{
+			Console.WriteLine("BASE: Printing()");
+		}
+
+	}
+
+	public class DerivedClass : BaseClass
+	{
+		public override void Print()
+		{
+			Console.WriteLine("DERIVED: Print() - override method");
+		}
+
+		public void Printing()
+		{
+			Console.WriteLine("DERIVED: Printing()");
+		}
+
+	}
+}
---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VirtualMethods;
+
+
+VirtualMethods();
+
+static void VirtualMethods()
+{
+	Console.WriteLine("BaseClass reference with BaseClass object");
+	BaseClass baseClass = new BaseClass();
+	baseClass.Print();
+	baseClass.Printing();
+	Console.WriteLine("");
+
+	Console.WriteLine("BaseClass reference with DerivedClass object");
+	BaseClass derived = new DerivedClass();
+	derived.Print();
+	derived.Printing();
+	Console.WriteLine("");
+
+	Console.WriteLine("BaseClass reference with DerivedClass object casted to BaseClass type");
+	BaseClass derived1 = (BaseClass)new DerivedClass();
+	derived1.Print();
+	derived1.Printing();
+	Console.WriteLine("");
+
+	/* OUTPUT *
+	  
+	  
+		BaseClass reference with BaseClass object
+		BASE: Print() - virtual method
+		BASE: Printing()
+
+		BaseClass reference with DerivedClass object
+		DERIVED: Print() - override method
+		BASE: Printing()
+
+		BaseClass reference with DerivedClass object casted to BaseClass type
+		DERIVED: Print() - override method
+		BASE: Printing()
+
+
+	/* OUTPUT */
+}
